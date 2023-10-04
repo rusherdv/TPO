0806a04407ae268eeca4bc53a93c0510e48c0049
Nicolas Montagut <nico.monta99@gmail.com>
2023-10-04 13:34:20 -0300

    evito que se carguen dos veces los jugadores, modifico botones para llamar a la api y llenar la plantillaagrego requires en el formulario de socio

--- public/inicio/js/script.js
+++ public/inicio/js/script.js
@@ -3,9 +3,13 @@ const btnPlayers = document.getElementById('jugadores');
 const btnTeam = document.getElementById('dataEquipo');
 
 window.onload = () => {
-  //getPlayersData();
-  //getTeamData();
-}
+  btnPlayers.addEventListener("click", () => {
+    getPlayersData();
+  });
+  btnTeam.addEventListener("click", ()=>{
+    getTeamData();
+  });
+};
 
 async function getTeamData() {
   await fetch("https://v3.football.api-sports.io/standings?league=128&season=2023", {
@@ -32,12 +36,12 @@ async function getPlayersData() {
       "x-rapidapi-key": "0212911083689fd2a01173883f35d27d"
     }
   })
-    .then(response =>{
+    .then(response => {
       response.json().then(data => {
-          data.response[0].players.forEach(player => {
-            const div = document.createElement('div')
-            div.classList.add('playerCard')
-            div.innerHTML = `
+        data.response[0].players.forEach(player => {
+          const div = document.createElement('div')
+          div.classList.add('playerCard')
+          div.innerHTML = `
             <div class="playerLeft">
             <img class="playerImage" src=${player.photo} alt="Imagen de jugador">
             </div>
@@ -48,12 +52,12 @@ async function getPlayersData() {
                 <p class="playerPosition">Posicion: <span>${player.position === 'Goalkeeper' ? 'Arquero' : player.position === 'Defender' ? 'Defensor' : player.position === 'Midfielder' ? 'Mediocentro' : player.position === 'Attacker' ? 'Delantero' : player.position}</span></p>
             </div>
             `
-            playerCardDiv.appendChild(div)
-          })
-          console.log(data.response[0].players)
-        }
+          playerCardDiv.appendChild(div)
+        })
+        console.log(data.response[0].players)
+      }
       )
-    } )
+    })
     .catch(err => {
       console.log(err);
     });
