f27256257d5f168954a9266a47c9c3c6bb000d64
Fede <fdecabanas2004@gmail.com>
2023-10-04 00:45:58 -0300

    Rediseño y terminado de pagina inicio, ademas de agregada la funcionalidad para obtener los jugadores de la plantilla (de momento se agrego un boton para asi no gastar los 100 requests diarios con tan solo actualizar la pagina, luego se quita y se hace solo)

--- inicio/js/script.js
+++ inicio/js/script.js
@@ -1,3 +1,5 @@
+const playerCardDiv = document.querySelector('.playerCardDiv')
+
 async function getTeamData(){
     await fetch("https://v3.football.api-sports.io/teams?id=459", {
       "method": "GET",
@@ -25,9 +27,38 @@ async function getPlayersData(){
     })
     .then(response => response.json())
     .then((data) => {
-      console.log(data)
+      data.response[0].players.forEach( player => {
+        const div = document.createElement('div')
+        div.classList.add('playerCard')
+        div.innerHTML = `
+        <div class="playerLeft">
+        <img class="playerImage" src=${player.photo} alt="Imagen de jugador">
+        </div>
+        <div class="playerRight">
+            <p class="playerName">${player.name}</p>
+            <p class="playerNum">Numero: <span>${player.number === null ? "Desconocido" : player.number}</span></p>
+            <p class="playerAge">Edad: <span>${player.age}</span></p>
+            <p class="playerPosition">Posicion: <span>${player.position === 'Goalkeeper' ? 'Arquero' : player.position === 'Defender' ? 'Defensor' : player.position === 'Midfielder' ? 'Mediocentro' : player.position === 'Attacker' ? 'Delantero' : player.position}</span></p>
+        </div>
+        `
+        playerCardDiv.appendChild(div)
+      })
+      console.log(data.response[0].players)
     })
     .catch(err => {
       console.log(err);
     });
+}
+
+async function getNews(){
+  await fetch("https://newsapi.org/v2/everything?q=afa&apiKey=12c7fdb8e00a4a55a9e81b85d44df9ae", {
+    "method": "GET"
+  })
+  .then(response => response.json())
+  .then((data) => {
+    console.log(data)
+  })
+  .catch(err => {
+    console.log(err);
+  });
 }
