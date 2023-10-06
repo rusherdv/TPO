e405fe88ceec3335c0a8719a6c96598cff0deab0
Fede <fdecabanas2004@gmail.com>
2023-10-06 15:26:44 -0300

    Tabla de puntos funcionando, se reestila estadisticas

--- public/stats/js/script.js
+++ public/stats/js/script.js
@@ -1,3 +1,5 @@
+const tbody = document.querySelector('tbody')
+
 async function getTeamData() {
   await fetch("https://v3.football.api-sports.io/standings?league=128&season=2023", {
     "method": "GET",
@@ -9,9 +11,29 @@ async function getTeamData() {
     .then(response => response.json())
     .then((data) => {
         data.response[0].league.standings[0].forEach(element => {
-            console.log('Equipo: ', element.team.name)
-            console.log('Logo equipo: ', element.team.logo)
-            console.log('Elemento: ', element)
+          console.log('Equipo: ', element.team.name)
+          console.log('Logo equipo: ', element.team.logo)
+          console.log('Elemento: ', element)
+          const tr = document.createElement('tr')
+          tr.innerHTML = `
+          <td>
+            <div class="team">
+              <p class="position">${element.rank}</p>
+              <img src=${element.team.logo} class="teamIcon" width="30" height="30" alt="">
+              <p class="teamName">${element.team.name}</p>
+            </div>
+          </td>
+          <td>${element.all.played}</td>
+          <td>${element.all.win}</td>
+          <td>${element.all.draw}</td>
+          <td>${element.all.lose}</td>
+          <td>${element.all.goals.for}</td>
+          <td>${element.all.goals.against}</td>
+          <td>${element.goalsDiff}</td>
+          <td>${element.points}</td>
+          `
+          
+          tbody.appendChild(tr)
         });
     })
     .catch(err => {
