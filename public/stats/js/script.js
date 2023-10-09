07d9cddfc1519c803b53fd9e530009ec7229e672
Fede <fdecabanas2004@gmail.com>
2023-10-09 01:26:22 -0300

    Se incorpora el panel de estadisticas del equipo

--- public/stats/js/script.js
+++ public/stats/js/script.js
@@ -1,6 +1,6 @@
 const tbody = document.querySelector('tbody')
 
-async function getTeamData() {
+async function getTableData() {
   await fetch("https://v3.football.api-sports.io/standings?league=128&season=2023", {
     "method": "GET",
     "headers": {
@@ -11,9 +11,6 @@ async function getTeamData() {
     .then(response => response.json())
     .then((data) => {
         data.response[0].league.standings[0].forEach(element => {
-          console.log('Equipo: ', element.team.name)
-          console.log('Logo equipo: ', element.team.logo)
-          console.log('Elemento: ', element)
           const tr = document.createElement('tr')
           tr.innerHTML = `
           <td>
@@ -34,6 +31,50 @@ async function getTeamData() {
           `
           
           tbody.appendChild(tr)
+          
+          if (element.team.name === 'Arsenal Sarandi'){
+            const container = document.querySelector('#stats')
+
+            const divPercents = document.querySelector('#percents')
+            const percents = document.createElement('div')
+            console.log(element)
+            percents.style = 'display: flex; justify-content: space-around; flex-direction: row;'
+            percents.innerHTML = `
+            <div class="rateDiv">
+            <p class="rateText">Victorias</p>
+            <h3 class="rate">%${Math.round(element.all.win*100/element.all.played)}</h5>
+            </div>
+            <div class="rateDiv">
+              <p class="rateText">Empates</p>
+              <h3 class="rate">%${Math.round(element.all.draw*100/element.all.played)}</h5>
+            </div>
+            <div class="rateDiv">
+              <p class="rateText">Derrotas</p>
+              <h3 class="rate">%${Math.round(element.all.lose*100/element.all.played)}</h5>
+            </div>
+            `
+
+            divPercents.appendChild(percents)
+            
+            const div = document.createElement('div')
+            div.innerHTML = `
+            <p class="textDiv">Partidos: <span>${element.all.played}</span></p>
+            <p class="textDiv">Ganados: <span>${element.all.win}</span></p>
+            <p class="textDiv">Empatados: <span>${element.all.draw}</span></p>
+            <p class="textDiv">Perdidos: <span>${element.all.lose}</span></p>
+            `
+
+            const div2 = document.createElement('div')
+            div2.innerHTML = `
+            <p class="textDiv">Goles metidos: <span>${element.all.goals.for}</span></p>
+            <p class="textDiv">Goles recibidos: <span>${element.all.goals.against}</span></p>
+            <p class="textDiv">Diferencia de goles: <span>${element.goalsDiff}</span></p>
+            <p class="textDiv">Puntos: <span>${element.points}</span></p>
+            `
+
+            container.appendChild(div)
+            container.appendChild(div2)         
+          }
         });
     })
     .catch(err => {
