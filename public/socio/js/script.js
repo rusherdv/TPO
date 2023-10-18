3d0f5a93233bc8ddb1be3b6faf253d8ab57287d9
Nicolas Montagut <nico.monta99@gmail.com>
2023-10-18 17:06:14 -0300

    agrego cambios para hacer mas tarde al script de socio form y cambio el max width de logo en navbar

--- public/socio/js/script.js
+++ public/socio/js/script.js
@@ -1,32 +1,7 @@
-// Script para validar el formulario de la página de contacto
-var name_ = document.getElementById('fullName'); // con _ porque generaba conflicto
+// TO DO Script para validar el formulario de la página de socio
+var name_ = document.getElementById('fullName'); 
+var numDoc = document.getElementById('docNum');
+var tel = document.getElementById('telefono');
 var email = document.getElementById('email');
-var pass = document.getElementById('password');
-
 var fail = document.getElementById('fail');
 var errorsAmount;
-/*function sendForm(){
-  errorsAmount = 0;
-  var errorMessages = [];
-
-  if (name_.value === null || name_.value === ""){
-    errorMessages.push("Debes ingresar tu nombre");
-    errorsAmount++;
-  }
-  if (pass.value === null || pass.value === ""){
-    errorMessages.push("Debes ingresar una contraseña");
-    errorsAmount++;
-  }
-  if (email.value === null || email.value === ""){
-    errorMessages.push("Debes ingresar una dirección de correo.");
-    errorsAmount++;
-  }
-
-  fail.innerHTML = errorMessages.join(", ");
-
-  if (errorsAmount === 0) {
-    alert("Enviado con éxito!");
-  }
-
-  return false;
-}*/
